184b000af45ff7ac319e6394758fec316e56b798
julian121266 <julianzilly@googlemail.com>
2018-11-01 16:24:35 +0100

    changing rosagent to use lane controller from control exercise

--- rosagent.py
+++ rosagent.py
@@ -10,9 +10,9 @@ class ROSAgent(object):
     def __init__(self):
         # Get the vehicle name, which comes in as HOSTNAME
         self.vehicle = os.getenv('HOSTNAME')
-        
+
         # Subscribes to the output of the lane_controller_node and IK node
-        self.action_sub = rospy.Subscriber('/{}/lane_controller_node/car_cmd'.format(
+        self.action_sub = rospy.Subscriber('/{}/lane_exercise_controller_node/car_cmd'.format(
             self.vehicle), Twist2DStamped, self._action_cb)
         self.ik_action_sub = rospy.Subscriber('/{}/wheels_driver_node/wheels_cmd'.format(
             self.vehicle), WheelsCmdStamped, self._ik_action_cb)
@@ -20,11 +20,11 @@ class ROSAgent(object):
         # Place holder for the action
         self.action = np.array([0, 0])
 
-        # Publishes onto the corrected image topic 
+        # Publishes onto the corrected image topic
         # since image out of simulator is currently rectified
         self.cam_pub = rospy.Publisher('/{}/corrected_image/compressed'.format(
             self.vehicle), CompressedImage, queue_size=10)
-        
+
         # Publisher for camera info - needed for the ground_projection
         self.cam_info_pub = rospy.Publisher('/{}/camera_node/camera_info'.format(
             self.vehicle), CameraInfo, queue_size=1)
@@ -43,7 +43,7 @@ class ROSAgent(object):
         """
         v = msg.v
         omega = msg.omega
-        
+
 
     def _ik_action_cb(self, msg):
         """
@@ -53,12 +53,12 @@ class ROSAgent(object):
         vl = msg.vel_left
         vr = msg.vel_right
         self.action = np.array([vl, vr])
-    
+
     def _publish_info(self):
         """
         Publishes a default CameraInfo - TODO: Fix after distortion applied in simulator
         """
-        self.cam_info_pub.publish(CameraInfo())      
+        self.cam_info_pub.publish(CameraInfo())
 
     def _publish_img(self, obs):
         """
@@ -73,8 +73,8 @@ class ROSAgent(object):
         img_msg.format = "jpeg"
         contig = cv2.cvtColor(np.ascontiguousarray(obs), cv2.COLOR_BGR2RGB)
         img_msg.data = np.array(cv2.imencode('.jpg', contig)[1]).tostring()
-  
-        self.cam_pub.publish(img_msg)    
+
+        self.cam_pub.publish(img_msg)
 
     def spin(self):
         """
